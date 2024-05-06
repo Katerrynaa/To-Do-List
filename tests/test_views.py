599931bea52f73f88d973abcb0bd9143bd4561de
Kateryna <katadmitrenkoxx@gmail.com>
2024-05-06 18:21:38 +0200

    Create view tests

--- tests/test_views.py
+++ tests/test_views.py
@@ -20,6 +20,49 @@ class TestCreateToDoView:
         manager_create_mock.assert_called_once_with(tasks=tasks_data)
 
     def test_result(self, create_task):
-        assert create_task.json() == {"message": "Successfully!"}
+        assert create_task.json() == { "message": "Successfully!" }
+
+
+
+class TestGetToDoView:
+    @fixture(scope="class")
+    def manager_get_mock(self):
+        with patch.object(ToDoManager, "get") as mock:
+            yield mock 
+
+    @fixture(scope="class", autouse=True)
+    def task_data(self, test_client, manager_get_mock):
+        return test_client.get("/get")
     
+    def test_manager_called(self, manager_get_mock):
+        manager_get_mock.assert_called_once_with()
+
+    def test_result(self, task_data):
+        assert task_data.json() == {}
+
+
+class TestGetToDoViewNotFound:
+    task_id = 1 
+
+    @fixture(scope="class")
+    def manager_get_by_id_mock(self):
+        with patch.object(ToDoManager, "get_by_id") as mock:
+            mock.return_value = None
+            yield mock 
+
+    @fixture(scope="class", autouse=True)
+    def get_by_id_todo(self, test_client, manager_get_by_id_mock):
+        return test_client.get(f"/get_by_id/{self.task_id}")
     
+    def test_manager_called(self, manager_get_by_id_mock):
+        manager_get_by_id_mock.assert_called_once_with(self.task_id)
+
+    def test_result(self, get_by_id_todo):
+        assert get_by_id_todo.json() == {
+            "detail": "Task with such ID not found"
+        }
+
+    def test_status_code(self, get_by_id_todo):
+        assert get_by_id_todo.status_code == 404
+
+
